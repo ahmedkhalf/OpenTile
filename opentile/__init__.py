fbe175feea108018c5fff4dc61d00115bd91ba64
Ahmed <ahmedkhalf567@gmail.com>
2020-09-01 00:34:33 +0400

    Add main loop and refactor code

--- opentile/__init__.py
+++ opentile/__init__.py
@@ -6,99 +6,64 @@ gi.require_version("Wnck", "3.0")
 from gi.repository import Wnck, GLib
 
 
-# def onWindowOpen(aScreen, aWindow):
-#     print("Window Opened!!")
+class TiledWorkspace:
+    def __init__(self, workspace):
+        self.width = workspace.get_width()
+        self.height = workspace.get_height()
+        self.windows = set()
+
+    def add_window(self, window):
+        self.windows.add(window)
+
+    def remove_window(self, window):
+        self.windows.remove(window)
 
 
 class Tiler:
     GRAVITY = Wnck.WindowGravity(0)
     RESIZE_MASK = Wnck.WindowMoveResizeMask(255)
 
-    def __init__(self, screen):
-        self.screen = screen
-        # self.screen.connect("window-opened", onWindowOpen)
-        self.width = screen.get_width()
-        self.height = screen.get_height()
+    def __init__(self):
+        self.screen = Wnck.Screen.get_default()
+        self.workspaces = {}
+
         self.outer_gap = 32
         self.inner_gap = 16
 
-    def get_active_window(self):
-        return self.screen.get_active_window()
+    def start(self):
+        self.screen.connect("workspace-created", self.on_workspace_created)
+        self.screen.connect("workspace-destroyed", self.on_workspace_destroyed)
+        self.screen.connect("window-opened", self.on_window_opened)
+        self.screen.connect("window_closed", self.on_window_closed)
 
-    def get_active_workspace(self):
-        return self.screen.get_active_workspace()
+        self.loop = GLib.MainLoop()
+        self.loop.run()
 
-    def get_windows(self):
-        return self.screen.get_windows()
+    def on_workspace_created(self, _screen, workspace):
+        number = workspace.get_number()
+        print("+ Workspace Created", number)
+        self.workspaces[number] = TiledWorkspace(workspace)
 
-    def get_workspace_windows(self, workspace):
-        windows = []
-        for window in self.get_windows():
-            if window.get_workspace() == workspace:
-                windows.append(window)
-        return windows
+    def on_workspace_destroyed(self, _screen, workspace):
+        number = workspace.get_number()
+        print("- Workspace Destroyed", number)
+        self.workspaces.pop(number)
 
-    def set_window_geometry(self, window, x, y, width, height):
-        window.set_geometry(Tiler.GRAVITY, Tiler.RESIZE_MASK, x, y - 30, width, height)
+    def on_window_opened(self, _screen, window):
+        print("-" * 40)
+        print("+ Window Opened", window.get_window_type())
+        self.workspaces[window.get_workspace().get_number()].add_window(window)
+        for workspace in self.workspaces:
+            print(self.workspaces[workspace].windows)
 
-    def tile(self):
-        active_workspace = self.get_active_workspace()
-        windows = self.get_workspace_windows(active_workspace)
-
-        master = self.get_active_window()
-        stack = [window for window in windows if window != master]
-
-        if master is None:
-            if len(stack) > 0:
-                master = stack[0]
-                stack.pop(0)
-            else:
-                return
-
-        # Resize Master Window
-        self.set_window_geometry(
-            master,
-            self.outer_gap,
-            self.outer_gap,
-            self.width // 2 - self.inner_gap // 2 - self.outer_gap
-            if len(stack) > 0
-            else self.width - self.outer_gap * 2,
-            self.height - self.outer_gap * 2,
-        )
-
-        # Resize Stack
-        for i, window in enumerate(stack):
-            self.set_window_geometry(
-                window,
-                self.width // 2 + self.inner_gap // 2,
-                self.outer_gap
-                + (
-                    (
-                        self.height
-                        - self.outer_gap * 2
-                        - self.inner_gap * (len(stack) - 1)
-                    )
-                    // len(stack)
-                )
-                * i
-                + self.inner_gap * i,
-                self.width // 2 - self.inner_gap // 2 - self.outer_gap,
-                (
-                    (
-                        self.height
-                        - self.outer_gap * 2
-                        - self.inner_gap * (len(stack) - 1)
-                    )
-                    // len(stack)
-                ),
-            )
+    def on_window_closed(self, _screen, window):
+        print("-" * 40)
+        print("- Window Closed")
+        self.workspaces[window.get_workspace().get_number()].remove_window(window)
+        for workspace in self.workspaces:
+            print(self.workspaces[workspace].windows)
 
 
-screen = Wnck.Screen.get_default()
-screen.force_update()
-
-tiler = Tiler(screen)
-tiler.tile()
-
-# loop = GLib.MainLoop()
-# loop.run()
+if __name__ == "__main__":
+    tiler = Tiler()
+    tiler.start()
